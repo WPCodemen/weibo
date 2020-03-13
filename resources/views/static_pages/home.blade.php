1877e8ff56fb073d91565be1e5d427e3c84459cf
Peng Wang <912157513@qq.com>
2020-03-13 16:32:52 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.default')
-@section('title', '首页')
 @section('content')
+
+	<div class="jumbotron"> 
+		 <h1>Hello Laravel</h1>
+		 <p class="lead">
+		 	你现在所看到的是  <a href="https://www.baidu.com">Larave  入门教程</a> 的实例项目主页
+		 </p>
+
+		 <p>
+		 	Everything will begin from there !!
+		 </p>
+
+		 <p>
+		 	<a class="btn btn-lg btn-success" href="#">现在注册</a>
+		 </p>
+
+	</div>
+
 	<h1>主页</h1>
 @stop
