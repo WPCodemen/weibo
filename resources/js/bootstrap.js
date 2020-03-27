beaad3d327f434acb0a780ff382d62914ae2713d
Peng Wang <912157513@qq.com>
2020-03-27 15:08:02 +0000

    登录成功后导航逻辑！

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,7 +10,14 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-/**
+try{
+	window.$ = window.jQuery = require('jQuery');
+
+	require('bootstrap');
+} catch (e) {}
+
+
+/**	
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
